88b4f3529e902be3610a0e7dd3acd28b787b8dae
CyberWo1fg <Den.vovk89@gmail.com>
2017-03-15 23:42:48 +0500

    minor starting programming

---
+++ SuperAdventureEditor/EditorForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace SuperAdventureEditor
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pMainPanel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pMainPanel, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(757, 387);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pMainPanel
+            // 
+            this.pMainPanel.AutoScroll = true;
+            this.pMainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pMainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pMainPanel.Location = new System.Drawing.Point(33, 33);
+            this.pMainPanel.Name = "pMainPanel";
+            this.pMainPanel.Size = new System.Drawing.Size(721, 351);
+            this.pMainPanel.TabIndex = 1;
+            // 
+            // EditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 387);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "EditorForm";
+            this.Text = "Редактор для игры SuperAdventure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel pMainPanel;
+    }
+}
+
---
+++ SuperAdventureEditor/EditorForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventureEditor
+{
+    public partial class EditorForm : Form
+    {
+        private const int bs = 60;
+        private int rowCount = 7;
+        private int colCount = 7;
+
+        public EditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pMainPanel.SuspendLayout();
+            pMainPanel.Controls.Clear();
+
+            for (int x = 0; x < colCount; x++)
+                for (int y = 0; y < rowCount; y++)
+                {
+                    PictureBox box = new PictureBox();
+                    //box.BackgroundImage = Properties.Resources.none;
+                    //box.BackgroundImageLayout = ImageLayout.Zoom;
+                    box.BorderStyle = BorderStyle.FixedSingle;
+                    box.Location = new Point(x * (bs) , y * (bs) );
+                    box.Size = new Size(bs, bs);
+                    box.SizeMode = PictureBoxSizeMode.Zoom;
+                    box.TabIndex = 0;
+                    box.TabStop = false;
+                    box.MouseClick += new MouseEventHandler(box_MouseClick);
+                    //box.MouseEnter += new EventHandler(picture_MouseEnter);
+                    //box.MouseLeave += new EventHandler(picture_MouseLeave);
+                    //box.MouseDown += new MouseEventHandler(picture_MouseDown);
+                    //box.MouseUp += new MouseEventHandler(picture_MouseUp);
+                    //box.MouseMove += new MouseEventHandler(picture_MouseMove);
+                    box.Tag = new Point(x, y);
+                    pMainPanel.Controls.Add(box);
+                    //boxes[x, y] = picture;
+                    //boxes[x, y].Image = CellToPicture(cells[x, y]);
+                }
+            pMainPanel.ResumeLayout();
+
+        }
+
+
+
+        private void box_MouseClick(object sender, EventArgs e)
+        {
+            int x = ((Point)((PictureBox)sender).Tag).X;
+            int y = ((Point)((PictureBox)sender).Tag).Y;
+            MessageBox.Show(x.ToString() + " " + y.ToString());
+
+        }
+    }
+}
