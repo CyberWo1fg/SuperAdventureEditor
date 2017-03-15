7f42a262676dd692259eeba3e784b715ef17dc04
CyberWo1fg <Den.vovk89@gmail.com>
2017-03-15 23:43:18 +0500

    rename form

--- SuperAdventureEditor/Program.cs
+++ SuperAdventureEditor/Program.cs
@@ -16,6 +16,7 @@ namespace SuperAdventureEditor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EditorForm());
         }
     }
 }
